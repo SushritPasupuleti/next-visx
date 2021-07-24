9314d889315040161d79e310e0519ff9bbf05e67
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2021-07-24 11:21:29 +0530

    Update _app.js

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,7 +1,31 @@
+import React from 'react';
+import Head from 'next/head';
 import '../styles/globals.css'
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <ThemeProvider>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, user-scalable=no, user-scalable=0"
+        />
+      </Head>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
-export default MyApp
+export default MyApp;
